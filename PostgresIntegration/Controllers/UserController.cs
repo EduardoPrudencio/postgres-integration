8b7eb50b21be0ad0ffddcdfbb194b16c5f0e2076
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2022-08-17 15:59:34 -0300

    Criando api de user

---
+++ PostgresIntegration/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PostgresIntegration.Database;
+
+namespace PostgresIntegration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        BusinessContext _context;
+        IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration, BusinessContext context)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return Ok("Tudo ok!");
+        }
+
+        [HttpPost]
+        public ActionResult Post(Model.User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
+
